3f9aa209d10399ba2482bb9f4c4e2a1ad5539f6b
Roberto Plancarte <robertoplancarte@gmail.com>
2013-03-31 23:01:18 -0600

    adds readme

---
+++ sepap.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+require 'json'
+
+@host = 'localhost'
+@port = '8000'
+
+i=10
+@payload ={'attempt_id'=>i,'attempt_ext'=>'c','compare'=>false,'command'=> "gcc -x c -o %s %s.c",'source'=>"\"#include<stdio.h>\\n\\nint main()\\n{\\n char string [256];\\n gets (string);\\n printf(\\\"%s\\\\n\\\",string);\\n return 0;\\n}\\n\"", 'cases'=> {'0'=>{ 'input'=>"#{0+i}\n", 'output' => "3\n"} ,'1'=>{ 'input'=>"#{1+i}\n", 'output' => "#{1+i}\n"} ,'2'=>{ 'input'=>"#{2+i}\n", 'output' => "#{2+i}\n"}}}.to_json
+
+def post
+  req = Net::HTTP::Post.new("/", initheader = {'Content-Type' =>'application/json'})
+  req.body = @payload
+  response = Net::HTTP.new(@host, @port).start {|http| http.request(req) }
+end
+
+post()
